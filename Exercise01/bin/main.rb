258669eefac1add560f33fdeddff42bb1cc0c428
Dayo Emmanuel <emmanuel.odunrintan@dealdey.com>
2015-11-30 17:05:02 +0100

    Count the ocurrences of various alphabet letters in an input string and store it in hash.

---
+++ Exercise01/bin/main.rb
@@ -0,0 +1,11 @@
+require_relative('../lib/UserInput.rb')
+
+puts "Please enter a text "
+text = gets.chomp
+userinput = UserInput.new()
+
+if text.nil?
+	puts "Please enter a text"
+else
+  userinput.get_occurences(text)
+end
