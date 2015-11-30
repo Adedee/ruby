74bbba1f3ad4eac6ca892aee38a7e927fb99bb89
Dayo Emmanuel <emmanuel.odunrintan@dealdey.com>
2015-11-30 15:10:12 +0100

    Write a method that sums up 2 24-hour time values(h:m:s). Validate the time using regular expressions

--- Exercise17/bin/main.rb
+++ Exercise17/bin/main.rb
@@ -1,9 +1,9 @@
 require_relative('../lib/SumTime.rb')
 
-puts "Please enter start time "
+puts "Please enter start time  "
 begin_time = gets.chomp
 
-puts "Please enter stop time "
+puts "Please enter stop time  "
 end_time = gets.chomp
  
 sumtime = SumTime.new(begin_time, end_time)
