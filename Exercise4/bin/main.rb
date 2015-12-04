4156a20426404f35421be7722d97b39a25372070
Dayo Emmanuel <emmanuel.odunrintan@dealdey.com>
2015-12-04 12:11:03 +0100

    Output whether the input string is palindrome. The program quits when user enters 'q/Q'.

--- Exercise4/bin/main.rb
+++ Exercise4/bin/main.rb
@@ -1,11 +1,11 @@
-require_relative('../lib/UserInput.rb')
+require_relative('../lib/user_input.rb')
 
 puts "Please enter a text"
 text = gets.chomp
 userinput = UserInput.new()
 
 if text.nil?
-	puts "Please enter a text"
+  puts "Please enter a text"
 else
   userinput.reverse_text(text)
 end
