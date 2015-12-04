73f003f102d4ab72d9ad8678c1c975af8ca953ae
Dayo Emmanuel <emmanuel.odunrintan@dealdey.com>
2015-12-04 15:20:07 +0100

    HashCreate a method for Array that can create a hash having 'key' as length of the element and value as an array of all the elements of that length. Make use of for-in loop for the purpose.

--- Exercise9/bin/main.rb
+++ Exercise9/bin/main.rb
@@ -1,4 +1,4 @@
-require_relative('../lib/ArrayHashClass.rb')
+require_relative('../lib/array_hash_class.rb')
 
 puts "Please enter number of array elements"
 text = gets.chomp
@@ -7,15 +7,14 @@ cnt = Float(text)
 arrnum = Array[]
 
 if text.nil?
-	puts "Please enter a new number"
+  puts "Please enter a new number"
 else
-
-	for i in 1..cnt
-    	puts "enter array value #{i}"
-    	text = gets.chomp
-   		arrnum.push(text)
-	end
- arrtohash = ArrayToHash.new(arrnum)
- puts arrtohash
+  for i in 1..cnt
+    puts "enter array value #{i}"
+    text = gets.chomp
+   	arrnum.push(text)
+  end
+  arrtohash = ArrayToHash.new(arrnum)
+  puts arrtohash
 
 end
---
+++ Exercise9/lib/array_hash_class.rb
@@ -0,0 +1,24 @@
+class ArrayToHash
+
+  def initialize(arr_num)
+     @array_number = arr_num
+  end
+
+   def to_s
+          arrlen = @array_number.size - 1
+          hash = Hash.new
+
+       each @array_number.each do |arrval|
+         
+          cnt = arrval.size
+          h1 = { cnt => arrval}
+          hash.merge!(h1)
+      end
+
+          puts "#{@array_number} \n"
+
+          puts "#{hash} \n"
+         
+    end
+
+end
