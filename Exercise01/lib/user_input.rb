ef004047415565d5aaf5ae9e135d469d47b17f28
Dayo Emmanuel <emmanuel.odunrintan@dealdey.com>
2015-12-04 11:56:04 +0100

    Count the ocurrences of various alphabet letters in an input string and store it in hash.

--- Exercise01/lib/user_input.rb
+++ Exercise01/lib/user_input.rb
@@ -3,12 +3,12 @@ class UserInput
   def get_occurences(word)
 	  hash = Hash.new(0)
   	  uword = word.upcase
-      ind = 0
+      
  	  uword.split("").each do |i|
       cnt = uword.count(i)
         if not hash.has_key?(i)
-           h1 = { i => cnt}
-           hash.merge!(h1)
+          h1 = { i => cnt}
+          hash.merge(h1)
         end 
     end
    puts "Output #{hash} "
