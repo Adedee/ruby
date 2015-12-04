55c88991b7b84192b263aa8dc0978b3ea69bb772
Dayo Emmanuel <emmanuel.odunrintan@dealdey.com>
2015-12-04 14:38:43 +0100

    Overwrite the default 'to_s' method such that it inverses the case of each letter.

--- Exercise7/bin/main.rb
+++ Exercise7/bin/main.rb
@@ -1,11 +1,11 @@
-require_relative('../lib/InverseClass.rb')
+require_relative('../lib/inverse_class.rb')
 
 puts "Please enter a text"
 text = gets.chomp
 inverse = Inverser.new(text)
 
 if text.nil?
-	puts "Please enter a text"
+  puts "Please enter a text"
 else
   inverse.to_s
 end
