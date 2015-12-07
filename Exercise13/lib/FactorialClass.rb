c43fb94448303bd3e090eb5c079bedbc1592f033
Dayo Emmanuel <emmanuel.odunrintan@dealdey.com>
2015-12-07 10:54:08 +0100

    Rewrite factorial method using ranges.

--- Exercise13/lib/FactorialClass.rb
+++ Exercise13/lib/FactorialClass.rb
@@ -4,14 +4,12 @@ class Factorial
      @num = text
 	end
 
-    def to_s
+  def to_s
     fnum = Float(@num)
     j = fnum
     (2..fnum).step(1).reverse_each do |i|
-       
-        j = j*(i - 1) 
-    end
-
+    j = j*(i - 1) 
+  end
     puts "#{fnum.to_i}! is #{j.to_i}"
   end
 end
