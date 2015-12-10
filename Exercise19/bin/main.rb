403af87cef5755c0ab5872fd911047ea32ded87c
Dayo Emmanuel <emmanuel.odunrintan@dealdey.com>
2015-12-10 14:35:43 +0100

    Sum TimeModify the sum of times method for any number of time values passed to this method.

--- Exercise19/bin/main.rb
+++ Exercise19/bin/main.rb
@@ -3,17 +3,17 @@ require_relative('../lib/SumTime.rb')
 puts "Please enter no of times to sum "
 x = gets.chomp
 
-timearr = Array[]
+time_array = Array[]
 if Float(x)
   0.step(x.to_i,1) do |num|
-  	puts "Enter time"
-  	 time_entry = gets.chomp
-     timearr.push(time_entry)
-	end    
+    puts "Enter time"
+    time_entry = gets.chomp
+    time_array.push(time_entry)
+  end    
 else
    puts "Invalid value"
-  Kernel.abort("Closing Program...")
+   Kernel.abort("Closing Program...")
 end
 
-sumtime = SumTime.new(timearr)
-puts sumtime.to_s
+sum_time = SumTime.new(time_array)
+puts sum_time.to_s
--- Exercise19/lib/SumTime.rb
+++ Exercise19/lib/SumTime.rb
@@ -1,84 +1,75 @@
 class SumTime
- @@time_var=0
-def initialize(time_arr)
- @array_times = time_arr
-end
-
-def to_s
-
-@array_times.each do |arrt|
-
-  if not arrt.match(/\d{2}:\d{2}:\d{2}/)
-  puts "Time does not match"
-  Kernel.abort("Closing Program...")
- end
-end
-
-time_addition(@array_times)
-convertToTime()
- 
-end
-
-def convert_to_seconds(time_var)
-
-  if time_var[0]==0
-  	hr = Float(time_var[1])
-  else
-	hr = Float(time_var[0..1])
-  end
-
-   if time_var[3]==0
-  	  min = Float(time_var[4])
-  else
-	  min = Float(time_var[3..4])
-  end
-
-   if time_var[6]==0
-  	  sec = Float(time_var[7])
-  else
-	  sec = Float(time_var[6..7])
-  end
-
-
- 
- 
-
-   hr = hr*60*60
-   min = min*60
-
-   converted_time = hr + min + sec
-   return converted_time
-end
-
-def time_addition(arr_times)
-
-
-   arr_times.each do |arr|
- arr_time = convert_to_seconds(arr)
+  @@time_variable=0
   
-  @@time_var = @@time_var + Float(arr_time)
-   end  
-end
+  def initialize(time_arr)
+    @array_times = time_arr
+  end
+
+  def to_s
+    @array_times.each do |array_time|
+
+    if not array_time.match(/\d{2}:\d{2}:\d{2}/)
+      puts "Time does not match"
+      Kernel.abort("Closing Program...")
+    end
+    time_addition(@array_times)
+    convert_to_time()
+  end
+
+  def convert_to_seconds(time_variable)
+
+    if time_variable[0]==0
+  	  hour = Float(time_variable[1])
+    else
+	    hour = Float(time_variable[0..1])
+    end
+
+    if time_variable[3]==0
+  	  minute = Float(time_variable[4])
+    else
+	    minute = Float(time_variable[3..4])
+    end
+
+    if time_variable[6]==0
+  	  seconds = Float(time_variable[7])
+    else
+	    seconds = Float(time_variable[6..7])
+    end
+
+    hour = hour*60*60
+    minute = minute*60
+
+    converted_time = hour + minute + seconds
+    return converted_time
+  end
+
+  def time_addition(arr_times)
+    arr_times.each do |arr|
+    arr_time = convert_to_seconds(arr)
+  
+    @@time_variable = @@time_variable + Float(arr_time)
+    end  
+  end
 
 
-def convertToTime()
-     timemod = @@time_var%3600
-         hr = @@time_var/3600
-       sec = timemod%60
-       min = timemod/60
+  def convert_to_time()
+    time_modulus = @@time_variable%3600
+    hour = @@time_variable/3600
+    seconds = time_modulus%60
+    minute = time_modulus/60
 
-       if hr > 24
-        days = hr/24
-        hr = hr%24
-       end
+    if hour > 24
+      days = hour/24
+      hour = hour%24
+    end
         
-       puts "Time Sum \n"
+    puts "Time Sum \n"
 
-       if hr > 24
-       puts "#{hr.to_i}:#{sec.to_i}:#{min.to_i}"
-      else
-        puts "#{days.to_i} day(s) #{hr.to_i}:#{sec.to_i}:#{min.to_i}"
-      end
-end
+    if hour > 24
+      puts "#{hour.to_i}:#{seconds.to_i}:#{minute.to_i}"
+    else
+      puts "#{days.to_i} day(s) #{hour.to_i}:#{seconds.to_i}:#{minute.to_i}"
+    end
+  end
 
 end
