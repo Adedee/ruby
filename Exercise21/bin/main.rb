5ed3422cbba343f10661a528c58971d5bfa0ebad
Dayo Emmanuel <emmanuel.odunrintan@dealdey.com>
2015-12-10 14:03:43 +0100

    FactorialAdd exception handling for negative numbers in factorial program.

--- Exercise21/bin/main.rb
+++ Exercise21/bin/main.rb
@@ -1,14 +1,15 @@
-require_relative('../lib/FactorialClass.rb')
+require_relative('../lib/factorial_class.rb')
 
 puts "Please enter factorial number "
 text = gets.chomp
 
 num = Float(text)
 if num < 0
-	Kernel.abort("Invalid number!")
+  Kernel.abort("Invalid number!")
 end
- characters = Factorial.new(num)
- puts characters.to_s
+
+characters = Factorial.new(num)
+puts characters.to_s
 
 
 
